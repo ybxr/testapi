c42b5312abffa10ab5c0e72df75858e15e189501
ybxr <remarkable2020@163.com>
2024-08-30 10:02:39 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,10 @@
 from flask import Flask
-
+ 
 app = Flask(__name__)
-
+ 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
+def hello():
+    return 'Hello, Vercel!'
+ 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
